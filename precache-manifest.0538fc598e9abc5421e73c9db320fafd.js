b9c9b77b091ea2c90500705ec779193442a44fb7
Barylo <dmitrobarilo@gmail.com>
2022-07-09 12:58:50 +0300

    Updates

---
+++ precache-manifest.0538fc598e9abc5421e73c9db320fafd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4c5f59db4cafb0aeaeff3144d401b2",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "ca90c3cd9119b990791e",
+    "url": "/ToDoList/static/js/2.e586d594.chunk.js"
+  },
+  {
+    "revision": "9eedd9e8f0af45c54c71",
+    "url": "/ToDoList/static/js/main.e1fe354c.chunk.js"
+  },
+  {
+    "revision": "78c0969e2ed717b7417d",
+    "url": "/ToDoList/static/js/runtime-main.b0953f8e.js"
+  }
+]);
